361c7a3bec6e62d89c2a7e8fab0779cb57a7d303
sauluiz <sauluisoliv@gmail.com>
2024-04-01 17:36:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using EntityFrameWorkEstudo1;
+
+namespace EntityFrameworkEstudo1{
+
+class Program{
+    static void Main(string[] args)
+    {
+            using(var baseDeDados= new AppDbContext()){
+                var cliente=new Cliente {Nome= "Saulo"};
+                baseDeDados.Clientes.Add(cliente);
+                baseDeDados.SaveChanges();
+            }
+    }
+}
+}
